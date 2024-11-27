659ff923f706732782cc3a80c56a8078e9b43217
wer-wersington <wersingtonsacramento@gmail.com>
2024-11-27 19:48:33 -0300

    Select atores

--- Projeto Catalogo Filmes/src/java/model/Ator.java
+++ Projeto Catalogo Filmes/src/java/model/Ator.java
@@ -6,12 +6,17 @@ package model;
 
 import Controller.ConectarDao;
 import Controller.IcrudDao;
+import com.mysql.cj.jdbc.Blob;
 import java.io.InputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,6 +30,14 @@ public class Ator extends ConectarDao implements IcrudDao {
     public InputStream capa; // Imagem guardada no InputStream
     public long capatamanho; // Guarda o tamanho da imagem em bytes
     public String capaimagemBase64;
+
+    public Ator(String nome, String fotinho) {
+        this.nomeAtor = nome;
+        this.capaimagemBase64 = fotinho;
+    }
+    
+    public Ator(){
+    }
     
     public void IncluirAtor(){
             try{setSql("INSERT INTO TB_ATOR (NM_ATOR, NR_IDADE, IMG_CAPA)"
@@ -58,7 +71,50 @@ public class Ator extends ConectarDao implements IcrudDao {
             this.setStatusSQL("Errro ao encontrar Ator ! <br>" + ex.getMessage());
         }
     }
+        
+        public ArrayList<Ator> listarAtores() {
 
+        ArrayList<Ator> atores = new ArrayList<>();
+
+        try {
+
+            sql = "SELECT * FROM TB_ATOR";
+
+            ps = con.prepareStatement(sql);
+
+            tab = ps.executeQuery();
+
+            String fotinho = "";
+
+            while (tab.next()) {
+                int id = tab.getInt(1);
+                String nome = tab.getString(2);
+                Blob blob = (Blob) tab.getBlob(3);
+
+                if (blob == null) {
+                    capaimagemBase64 = null;
+                } else {
+                    capa = blob.getBinaryStream();
+                    byte[] buffer = new byte[(int) blob.length()];
+                    capa.read(buffer);
+                    fotinho = capaimagemBase64 = Base64.getEncoder().encodeToString(buffer);
+                }
+
+                atores.add(new Ator(nome, fotinho));
+            }
+
+            this.statusSQL = null;
+        } catch (SQLException ex) {
+            this.statusSQL = "Erro ao incluir usuario ! <br> " + ex.getMessage();
+        } catch (IOException ex) {
+            Logger.getLogger(Filme.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return atores;
+    }
+
+        
+        
     public String getNomeAtor() {
         return nomeAtor;
     }
